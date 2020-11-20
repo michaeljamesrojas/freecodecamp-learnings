a62c8028f5ecefa69243aedf54308a728d2744f2
Michael James Rojas <michaeljamesrojas123@gmail.com>
2020-11-20 14:41:52 +0800

    edit

--- responsive-web-design-certification/myScript.js
+++ responsive-web-design-certification/myScript.js
@@ -1,1 +1,1 @@
-document.getElementById("warning").innerHTML = "click: -> EXPERIMENTAL <- (ALL DIV AND FLEXIBLE or GRID)";
+document.getElementById("warning").innerHTML = "click: -> EXPERIMENTAL <- (ALL DIV AND FLEXIBLE and or GRID (grid template areas)(1fr 2fr))";
